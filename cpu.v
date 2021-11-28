b645edf59b2bbe04dfc0d0d168dbab7bfc23e68b
aleksStajic <sstajic10@gmail.com>
2021-11-27 17:42:17 -0800

    Implemented new hardware blocks for CPU and lab7_top

--- cpu.v
+++ cpu.v
@@ -1,8 +1,14 @@
-module cpu(clk,reset,s,load,in,out,N,V,Z,w);
+module cpu(clk,reset,s,load,in,out,N,V,Z, mem_cmd, mem_addr);
     input clk, reset, s, load;
     input [15:0] in;
     output [15:0] out;
     output N, V, Z, w;
+
+    //Memory outputs
+    output [1:0] mem_cmd;
+    output [8:0] mem_addr;
+    //--------------------
+    
     wire [2:0] opcode_out;
     wire [1:0] op_out;
     wire [1:0] shift_out;
@@ -12,10 +18,17 @@ module cpu(clk,reset,s,load,in,out,N,V,Z,w);
     wire [2:0] readnum_out;
     wire [2:0] writenum_out;
     wire [1:0] nsel_fsm; // output from fsm
-
     wire [15:0] IR_out;
-
     wire [8:0] dp_cntrl;
+
+    //Memory wires
+    wire load_pc;
+    wire [8:0] next_pc;
+    wire [8:0] PC;
+    wire reset_pc;
+    wire addr_sel;
+    wire [8:0] mem_addr;
+    wire [1:0] mem_cmd;
 
     datapath DP(.clk(clk), // recall from Lab 4 that KEY0 is 1 when NOT pushed
 
@@ -72,4 +85,28 @@ module cpu(clk,reset,s,load,in,out,N,V,Z,w);
                     .DP_CNTRL(dp_cntrl)
                  );
 
+    
+    vDFFE_PC PCREG(clk, load_pc, next_pc, PC); //instantiating program counter register with load enable
+
+    assign next_pc = reset_pc ? 9'd0 : PC + 1'b1; //intantiating PC multiplexer
+
+    assign mem_addr = addr_sel ? PC : 9'd0;
+
+
+endmodule
+
+//This module was copied from the example
+//on the Lab5 example slides
+module vDFFE_PC(clk, en, in, out) ;
+  parameter n = 1;  // width
+  input clk, en ;
+  input  [n-1:0] in ;
+  output [n-1:0] out ;
+  reg    [n-1:0] out ;
+  wire   [n-1:0] next_out ;
+
+  assign next_out = en ? in : out;
+
+  always @(posedge clk)
+    out = next_out;  
 endmodule
---
+++ lab7_top.v
@@ -0,0 +1,52 @@
+`define MNONE 2'b00
+`define MREAD 2'b01
+`define MWRITE 2'b10
+
+
+module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
+    input [3:0] KEY;
+    input [9:0] SW;
+    output [9:0] LEDR;
+    output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+
+    //Memory input/output
+    wire [15:0] read_data;
+    wire [15:0] dout_bus;
+    wire [15:0] cpu_out;
+    wire N, V, Z;
+    wire [1:0] mem_cmd_bus;
+    wire [8:0] mem_addr_bus;
+    wire enable_tri;
+
+    RAM MEM(~KEY[0], mem_addr_bus[7:0], mem_addr_bus[7:0], 0, 0, dout_bus); //instantiating RAM
+    cpu CPU(~KEY[0], ~KEY[1], ~KEY[2], ~KEY[3], read_data, cpu_out, N, V, Z, mem_cmd_bus, mem_addr_bus); //CPU instantiation
+
+    assign enable_tri = (`MREAD === mem_cmd_bus) & (mem_addr_bus[8] === 1'b0); //AND gate input to tri-state driver
+    assign read_data = enable_tri ? dout_bus : {16{1'bz}}; //setting up tri-state driver logic    
+
+endmodule
+
+module RAM(clk,read_address,write_address,write,din,dout);
+  parameter data_width = 32; 
+  parameter addr_width = 4;
+  parameter filename = "data.txt";
+
+  input clk;
+  input [addr_width-1:0] read_address, write_address;
+  input write;
+  input [data_width-1:0] din;
+  output [data_width-1:0] dout;
+  reg [data_width-1:0] dout;
+
+  reg [data_width-1:0] mem [2**addr_width-1:0];
+
+  initial $readmemb(filename, mem);
+
+  always @ (posedge clk) begin
+    if (write)
+      mem[write_address] <= din;
+    dout <= mem[read_address]; // dout doesn't get din in this clock cycle 
+                               // (this is due to Verilog non-blocking assignment "<=")
+  end 
+endmodule
+
